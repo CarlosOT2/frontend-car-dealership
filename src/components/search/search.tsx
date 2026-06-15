f24aaac82e64fcac2db4d9166607caf2640bbbf5
CarlosV <135380086+CarlosOT2@users.noreply.github.com>
2026-06-15 17:04:24 -0300

    Add form controller and refactor layout structure
    - Add Input component and useFormController hook to Search
    - Introduce handleSubmit async function wired to SubmitController
    - Replace height % on __tabs with margin-based spacing
    - Remove padding/margin-top from __tabs-list in favor of parent margin
    - Add no_select prop to tab Text components

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,23 @@
 //# Components //
 import Img from '../global/img'
 import Text from '../global/text'
+import Input from '../global/inputs/input'
 //# Libs //
 import { useTranslation } from 'react-i18next'
+//# Utils //
+import { useFormController } from '../../shared/form/FormController'
 //# Classes //
 import './search.scss'
 
 export default function Search() {
+    const { InputsController, SubmitController } = useFormController({
+        handleSubmit: handleSubmit
+    })
     const { t } = useTranslation()
+
+    async function handleSubmit() {
+
+    }
 
     return (
         <section className='search'>
@@ -25,28 +35,35 @@ export default function Search() {
                         <nav className='search__section-form__tabs'>
                             <ul className='search__section-form__tabs-list'>
                                 <li className='search__section-form__tabs-item'>
-                                    <Text tag='span' className='search__section-form__tabs-text'>
+                                    <Text tag='span' className='search__section-form__tabs-text' no_select={true}>
                                         Veículos Novos
                                     </Text>
                                 </li>
                                 <li className='search__section-form__tabs-item'>
-                                    <Text tag='span' className='search__section-form__tabs-text'>
+                                    <Text tag='span' className='search__section-form__tabs-text' no_select={true}>
                                         Veículos Semi-Novos
                                     </Text>
                                 </li>
                                 <li className='search__section-form__tabs-item'>
-                                    <Text tag='span' className='search__section-form__tabs-text'>
+                                    <Text tag='span' className='search__section-form__tabs-text' no_select={true}>
                                         Acessórios
                                     </Text>
                                 </li>
                             </ul>
                         </nav>
 
-                        <form className='search__section-form__form'>
-
+                        <form
+                            className='search__section-form__form'
+                            onSubmit={SubmitController.onSubmit}
+                        >
+                            <Input
+                                type='text'
+                                name='teste'
+                                autoComplete='off'
+                                InputsController={InputsController}
+                            />
                         </form>
                     </div>
-
                 </section>
             </div >
         </section >
